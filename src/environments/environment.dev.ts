2ee8f17c2198df31dc528797fe8ca0e6d36e8087
Michael Ansah <michael.ansah@nov.com>
2024-07-09 10:16:04 +0100

    environment.dev added to gitignore

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,15 @@
+export const dev = {
+  // apiUrl: 'http://127.0.0.1:5008/api/',
+  apiUrl: 'http://127.0.0.1:5008',
+  staticUrl: 'http://127.0.0.1:5008/static/',
+  socketEndpoint: 'http://localhost:3000/',
+  oktaIssuer: '',
+  oktaClientId: '',
+  // oktaIssuer: '',
+  // oktaClientId: ''
+  // wsEndpoint: '',
+  meiliSearch: {
+    host: '',
+    apiKey: '',
+  },
+};
